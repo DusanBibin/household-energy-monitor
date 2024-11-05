73680d55977deefe8238d31adfc3232876964c8b
DusanBibin <dusanbibin2@gmail.com>
2024-11-06 00:16:34 +0100

    Configured docker compose for postgresql

---
+++ nvtbackend/src/main/java/com/example/nvt/NvtApplication.java
@@ -0,0 +1,14 @@
+package com.example.nvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvtApplication.class, args);
+		
+	}
+
+}
---
+++ nvtbackend/src/test/java/com/example/nvt/NvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
